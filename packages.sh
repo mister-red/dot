2a4bb23a0b595e51b9cca6362c8b9b3ada2b2753
mister-red <carty.dj@gmail.com>
2018-10-15 22:42:06 -0500

    added package installer script

---
+++ packages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# A simple script that installs all the packages I need for the configuration files in this repo
+
+pacman -S \
+	base-devel \
+	binutils \
+	awesome \
+	neovim \
+	rxvt-unicode \
+	firefox
