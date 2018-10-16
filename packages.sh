e3ce4344f4bbfd4565ff6a8cc55f3bee1fbe103a
mister-red <carty.dj@gmail.com>
2018-10-15 22:58:04 -0500

    added more packages

--- packages.sh
+++ packages.sh
@@ -2,10 +2,23 @@
 #
 # A simple script that installs all the packages I need for the configuration files in this repo
 
+echo "== Attempting to install core packages =="
 pacman -S \
 	base-devel \
 	binutils \
-	awesome \
+	xorg-server \
+	xorg-xinit
+
+echo "== Attempting to install Nvidia packages =="
+pacman -S \
+	nvidia \
+	nvidia-utils \
+	nvidia-settings
+
+echo "== Attempting to install other packages =="
+pacman -S \
 	neovim \
 	rxvt-unicode \
+	slock xautolock \
+	awesome \
 	firefox
